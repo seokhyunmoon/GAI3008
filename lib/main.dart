3c2ec6cdcd506aeb7335d7479abf4d7d4f0d7979
SeokHyun Moon <seokhyunmoon@yonsei.ac.kr>
2023-10-28 15:07:03 +0900

    add subpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,9 +18,123 @@ class MyApp extends StatelessWidget {
   }
 }
 
-@override
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
+ 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class Subpage extends StatelessWidget {
+  const Subpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xffffffff),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Color(0xff212435),
+          size: 24,
+        ),
+        actions: const [
+          Icon(Icons.add_box, color: Color(0xff212435), size: 24),
+        ],
+      ),
+      
+      
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan Recipt',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payments),
+            label: 'Send & Request',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: 3,
+        elevation: 0,
+        iconSize: 25,
+        selectedItemColor: const Color(0xff3a57e8),
+        unselectedItemColor: const Color(0xff9e9e9e),
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (int index) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Homepage()
+            )
+          );
+        },
+      ),
+      
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 70, 0, 30),
+            child:
+              Image(
+              image: AssetImage("assets/images/QR.png"),
+              height: 150,
+              width: 150,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+            child: Text(
+              "SeokHyun Moon",
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.clip,
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontStyle: FontStyle.normal,
+                fontSize: 25,
+                color: Color(0xff000000),
+              ),
+            ),
+          ),
+          Text(
+            "Scan to pay @seokhyunmoon",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.clip,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              fontSize: 12,
+              color: Color(0xff000000),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _MyHomePageState extends State<Homepage> {
+  int selectedItemIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +168,47 @@ class Homepage extends StatelessWidget {
       ),
 
       /*
+      Bottom Navigation Bar
+      */
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.qr_code_scanner),
+            label: 'Scan Recipt',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payments),
+            label: 'Send & Request',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: selectedItemIndex,
+        elevation: 0,
+        iconSize: 25,
+        selectedItemColor: const Color(0xff3a57e8),
+        unselectedItemColor: const Color(0xff9e9e9e),
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (int index) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Subpage()
+            )
+          );
+
+        },
+      ),
+
+      /*
       Body
       */
 
@@ -65,72 +221,73 @@ class Homepage extends StatelessWidget {
           /*
           상단 Row
           */
+          Expanded(
+            flex: 0,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(80, 10, 80, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      margin: const EdgeInsets.all(0),
+                      padding: const EdgeInsets.all(0),
+                      decoration: const BoxDecoration(
+                        color:  Color(0xffffffff),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.zero,
+                      ),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.all(0),
-                    padding: const EdgeInsets.all(0),
-                    decoration: const BoxDecoration(
-                      color:  Color(0xffffffff),
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.zero,
-                    ),
-
-                    /*
-                    이름 + Insights
-                    */
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text(
-                          "Hi SeokHyun,",
-                          textAlign: TextAlign.start,
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w900,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 16,
-                            color: Color(0xff000000),
+                      /*
+                      이름 + Insights
+                      */
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Text(
+                            "Hi SeokHyun,",
+                            textAlign: TextAlign.start,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 16,
+                              color: Color(0xff000000),
+                            ),
                           ),
-                        ),
-                        Text(
-                          "What do you want to do today?",
-                          textAlign: TextAlign.start,
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 12,
-                            color: Color(0xff000000),
+                          Text(
+                            "What do you want to do today?",
+                            textAlign: TextAlign.start,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 12,
+                              color: Color(0xff000000),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                  child: IconButton(
-                    icon: const Icon(Icons.addchart),
-                    onPressed: () {},
-                    color: const Color(0xff9fc7ff),
-                    iconSize: 35,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: IconButton(
+                      icon: const Icon(Icons.insights),
+                      onPressed: () {},
+                      color: const Color(0xff9fc7ff),
+                      iconSize: 35,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-
           /*
           하단 Row
           */
@@ -220,7 +377,7 @@ class Homepage extends StatelessWidget {
                             Padding(
                               padding: EdgeInsets.fromLTRB(70, 10, 10, 50),
                               child: Icon(
-                                Icons.account_balance_wallet,
+                                Icons.history_edu,
                                 color: Color(0xffff3d3d),
                                 size: 30,
                               ),
@@ -411,7 +568,7 @@ class Homepage extends StatelessWidget {
                             Padding(
                               padding: EdgeInsets.fromLTRB(70, 10, 10, 50),
                               child: Icon(
-                                Icons.more,
+                                Icons.sell,
                                 color: Color(0xff764aff),
                                 size: 30,
                               ),
@@ -470,9 +627,9 @@ class Homepage extends StatelessWidget {
                             Padding(
                               padding: EdgeInsets.fromLTRB(70, 10, 10, 38),
                               child: Icon(
-                                Icons.transform,
+                                Icons.compare_arrows,
                                 color: Color(0xff1a3cff),
-                                size: 30,
+                                size: 33,
                               ),
                             ),
                             Padding(
